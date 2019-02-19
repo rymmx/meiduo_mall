09bfc59161a8095c92f3c2c55d6b90d6ddec249f
日月木目心 <630438762@qq.com>
2019-02-19 08:51:16 +0800

    获取请求体中非表单数据,request参数传递过程理解

--- request_response/urls.py
+++ request_response/urls.py
@@ -4,5 +4,14 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^weather/[a-z]+/\d{4}/$', views.weather1),
+    # 演示提取url路径参数
+    # 这里必须分组,和老师演示不一样
+    # 演示利用正则组提取url路径参数(位置参数)
+    url(r'^weather1/([a-z]+)/(\d{4})/$', views.weather1),
+
+    # 演示利用正则组起别名提取url路径参数(关键字参数)
+    url(r'^weather2/(?P<city>[a-z]+)/(?P<year>\d{4})/$', views.weather2),
+
+    # 演示获取请求json数据
+    url(r'^get_body_json/$', views.get_body_json),
 ]
--- request_response/views.py
+++ request_response/views.py
@@ -1,11 +1,30 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
+# GET  /weather1/beijing/2018
 def weather1(request, city, year):
 
     print(city)
     print(year)
     return HttpResponse('weather1')
+
+
+# GET  /weather2/beijing/2018
+def weather2(request, year, city):
+    # print(request.user)  # AnonymousUser 匿名用户
+    print(city)
+    print(year)
+    return HttpResponse('weather2')
+
+
+# 演示获取请求体中非表单数据
+def get_body_json(request):
+    json_str_bytes = request.body
+    json_str = json_str_bytes.decode()
+    dict1 = json.loads(json_str)
+    return HttpResponse('get_body_json')
