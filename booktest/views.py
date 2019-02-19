3af8ed098ac5ebafde1e68b2c6e26b2a30db6ddd
日月木目心 <630438762@qq.com>
2019-02-19 17:22:05 +0800

    ORM操作

--- booktest/views.py
+++ booktest/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from .models import BookInfo,HeroInfo
 # Create your views here.
+
+# 第一种写法
+book = BookInfo()
+book.btitle = "三国"
+book.bpub_date = "1995-11-11"
+book.save()
+
+# 第二种写法
+book2 = BookInfo(
+    btitle="小三国",
+    bpub_date="1995-11-11"
+)
+book2.save()
+
+# 第三种写法
+book3 = BookInfo.objects.create(
+    btitle="西游记",
+    bpub_date="1998-11-11"
+)
+
+
+hero = HeroInfo(
+    hname="孙悟空",
+    # hbook=book3,  # 外键的关联赋值
+    hbook_id=book3.id,
+)
