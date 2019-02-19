696668561ffbceeee293526c7af9afea45dcd970
日月木目心 <630438762@qq.com>
2019-02-19 12:06:02 +0800

    自定义装饰器,以及django自带装饰器,解决自定义装饰器传参错乱问题

--- classview/urls.py
+++ classview/urls.py
@@ -5,6 +5,13 @@ urlpatterns = [
 
     # url(路由正则,视图函数名称)
     # as_view 将方法变成函数  dispatch 根据请求方法动态查找类视图中的方法
+    # 如果把装饰器直接写在定义路由的地方,会把类视图中所有的请求都装饰
     url(r'^demoview/$', views.DemoView.as_view()),
+
+    # 自定义装饰器测试路由
+    url(r'^view_func/$', views.view_func),
+
+    # django自带装饰器测试
+    url(r'^TestView/$', views.TestView.as_view()),
 
 ]
--- classview/views.py
+++ classview/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views import View
+
 
 # Create your views here.
-from django.views import View
 
 
 class DemoView(View):
@@ -17,3 +19,40 @@ class DemoView(View):
 
     def post(self,request):
         return HttpResponse('post请求业务逻辑')
+
+
+def my_decorator(view_func):
+    """
+    装饰器
+    :param view_func: 被装饰的函数
+    :return: wrapper
+    """
+
+    def wrapper(request, *args, **kwargs):
+        print('my_decorator装饰器被调用了')
+        print(request.method, request.path)
+        # 调用被装饰的函数视图
+        return view_func(request, *args, **kwargs)
+
+    return wrapper
+
+
+@my_decorator
+def view_func(request):
+    return HttpResponse('view_func')
+
+
+# django自带装饰器,解决自定义装饰器传参错乱问题
+@method_decorator(my_decorator, name='get')
+class TestView(View):
+    """类视图的定义和使用"""
+
+    def get(self, request):
+        """GET请求业务逻辑"""
+        return HttpResponse('get')
+
+    @method_decorator(my_decorator)  # 装饰类中的方法
+    def post(self, request):
+        """POST请求业务逻辑"""
+        return HttpResponse('post')
+
